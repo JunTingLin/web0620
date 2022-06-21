ba12d042941a6d473a96acfc5e2d37cef100a0f7
JunTingLin <10355098@st.thsh.tp.edu.tw>
2022-06-22 00:51:40 +0800

    RequestDispatcher

---
+++ myWeb/src/main/java/com/gjun/controllor/RegisterFormServlet.java
@@ -0,0 +1,23 @@
+package com.gjun.controllor;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/regform")
+public class RegisterFormServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher disp = 
+				req.getRequestDispatcher("WEB-INF/views/usersform.jsp");
+		disp.forward(req, resp);
+		
+	}
+	
+
+}
