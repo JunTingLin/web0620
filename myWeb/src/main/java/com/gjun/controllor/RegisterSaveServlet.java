bc3c554e73c43df7766918fccd6fe9d1a557d04a
JunTingLin <10355098@st.thsh.tp.edu.tw>
2022-06-23 00:37:22 +0800

    register DAO

--- myWeb/src/main/java/com/gjun/controllor/RegisterSaveServlet.java
+++ myWeb/src/main/java/com/gjun/controllor/RegisterSaveServlet.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import com.gjun.beans.Users;
+import com.gjun.domain.IDao;
+import com.gjun.domain.UserDao;
 
 /**
  * Servlet implementation class RegisterSaveServlet
@@ -40,25 +42,22 @@ public class RegisterSaveServlet extends HttpServlet {
 		users.setPassword(password);
 		users.setRealName(realName);
 		users.setEmail(email);
-		
 		//可參考應用系統聆聽器ApplicationHandler裡application.setAttribute，活得比較久共用
 		//這裡request死掉，我的users javaBean就釋放
 		request.setAttribute("users", users);
 		
-		DataSource datasource = (DataSource) this.getServletContext().getAttribute("datasource");
+		DataSource datasource = 
+				(DataSource) this.getServletContext().getAttribute("datasource");
+		IDao<Users> dao = new UserDao();
+		dao.setDataSource(datasource);
 		try {
-			Connection connection = datasource.getConnection();
-			String sql = "INSERT INTO javausers(username,password,realname,email) VALUES(?,?,?,?)";
-			PreparedStatement st =connection.prepareStatement(sql);
-			st.setString(1, users.getUserName());
-			st.setString(2, users.getPassword());
-			st.setString(3, users.getRealName());
-			st.setString(4, users.getEmail());
-			int r = st.executeUpdate();
+			dao.insert(users);
 		} catch (SQLException e) {
-			
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 		RequestDispatcher disp = request.getRequestDispatcher("WEB-INF/views/showresult.jsp");
 		disp.forward(request, response);
 		
