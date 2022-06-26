ba566deb6332f49fa1fa659f1c6ab6de310c93cc
JunTingLin <10355098@st.thsh.tp.edu.tw>
2022-06-26 19:47:28 +0800

    @Context get datasource

--- myWeb/src/main/java/com/gjun/service/CustomersService.java
+++ myWeb/src/main/java/com/gjun/service/CustomersService.java
@@ -1,9 +1,16 @@
 package com.gjun.service;
 
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.sql.DataSource;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 
 @Path("/Customers")
 public class CustomersService {
@@ -11,7 +18,17 @@ public class CustomersService {
 	@GET
 	@Produces("text/plain")
 	//傳遞國家別 進行相關客戶查詢服務 採用QueryString傳遞國家別../custqry/rawdata?country=xxx
-	public String customersQry(@QueryParam("country")String country) {
-		return country;
+	public String customersQry(@Context ServletContext application,@QueryParam("country")String country) {
+		DataSource datasource = (DataSource) application.getAttribute("datasource");
+		String dbName = null;
+		try {
+			Connection connection = datasource.getConnection();
+			dbName=connection.getCatalog();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return dbName;
 	}
+	
 }
